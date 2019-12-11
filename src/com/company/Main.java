b45102eea9538e906d4fe7a4e4cdac675adc7008
David-1996 <david-mkrtchyan-96@mail.ru>
2019-12-11 17:52:31 +0400

    1_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int number1 = 50;
-        int number2 = 3;
-        int result = number1 / number2;
+        int number1 = -5;
+        int number2 = 8;
+        int number3 = 6;
+        int result = number1 + number2 * number3;
         System.out.println(result);
     }
 }
