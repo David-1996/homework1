2129e6eaa3c9934bace8af8e4ac73fb49807af15
David-1996 <david-mkrtchyan-96@mail.ru>
2019-12-11 17:46:08 +0400

    1_1

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number1 = 74;
+        int number2 = 36;
+        int result = number1 + number2;
+        System.out.println(result);
+    }
+}
